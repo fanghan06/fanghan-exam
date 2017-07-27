ea7b5c14ee7dc171879d6bfad605a23dbe042205
fanghan <fhfanghan@126.com>
2017-07-27 18:06:40 +0800

    first commit

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  users:Array<any> = [
+  public users: Array<any> = [
     {"index":19,
     "name":"刘雨飏",
       "sex":"Male",
@@ -31,7 +31,23 @@ export class HomePageComponent implements OnInit {
       "github":"chunk",
       "exam1":76,
       "exam2":76,
-      "exam3":76}
+      "exam3":76},
+      {"index":88,
+      "name":"Frank",
+      "sex":"Male",
+      "name_en":"Frank",
+      "github":"Frank",
+      "exam1":78,
+      "exam2":78,
+      "exam3":78},
+      {"index":41,
+      "name":"小红",
+      "sex":"Female",
+      "name_en":"Hong",
+      "github":"Hong",
+      "exam1":86,
+      "exam2":86,
+      "exam3":86}
   ]
 deleteLast(){
   this.users.pop()
@@ -47,17 +63,31 @@ saveNewUser(){
       "exam2":76,
       "exam3":76
   })
-}
+};
   sortByAsccending(){
     // 参考MDN Array操作的API文档 Array相关方法方法
+    this.users.sort(function(a,b){
+        return a.index-b.index});
   }
   sortByDesccending(){
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
+    this.users.sort(function(a,b){
+        return a.index-b.index}).reverse();
   }
   sortByRadom(){
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+    function shuffle(a) {
+      for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+        console.log("i是"+i);
+        console.log("j是"+j);
+        console.log(a.index);
+      }
+    }
+    shuffle(this.users);
   }
   constructor(meta: Meta, title: Title) {
     title.setTitle('My Home Page');
