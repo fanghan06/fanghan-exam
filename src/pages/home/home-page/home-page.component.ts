f179ae14ba93476ad3ea16de5bd8d55c2d3aec89
fanghan <fhfanghan@126.com>
2017-07-29 13:08:40 +0800

    version：0.2

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { ProgramsService } from '../programs.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,93 +8,49 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  public users: Array<any> = [
-    { "index":19,
-      "name": "刘雨飏",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
-      {"index":7,
-      "name":"小明",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
-      {"index":14,
-      "name":"Chunk",
-      "sex":"Male",
-      "name_en":"Chunk",
-      "github":"chunk",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76},
-      {"index":88,
-      "name":"Frank",
-      "sex":"Male",
-      "name_en":"Frank",
-      "github":"Frank",
-      "exam1":78,
-      "exam2":78,
-      "exam3":78},
-      {"index":41,
-      "name":"小红",
-      "sex":"Female",
-      "name_en":"Hong",
-      "github":"Hong",
-      "exam1":86,
-      "exam2":86,
-      "exam3":86}
-  ]
-deleteLast(){
-  this.users.pop()
-}
-saveNewUser(){
-  this.users.push({
-     "index":666,
-      "name":"New User",
-      "sex":"Male",
-      "name_en":"new_user",
-      "github":"new_user",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76
-  })
-};
-  sortByAsccending(){
-    // 参考MDN Array操作的API文档 Array相关方法方法
-    this.users.sort(function(a,b){
-        return a.index-b.index});
+   private programs: Array<any> = [];
+
+  deleteLast() {
+    this.programs.pop();
   }
-  sortByDesccending(){
+  saveNewUser() {
+    this.programs.push({
+      'id': 6,
+      'name': '演唱《小幸运》',
+      'member': 'Liferay',
+      'number': '1',
+      'time': '20:10',
+      'grade': 84
+    });
+  }
+  sortByAsccending() {
+    // 参考MDN Array操作的API文档 Array相关方法方法
+    this.programs.sort(function (a, b) {
+      return a.id - b.id;
+    });
+  }
+  sortByDesccending() {
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
-    this.users.sort(function(a,b){
-        return a.index-b.index}).reverse();
+    this.programs.sort(function (a, b) {
+      return a.id - b.id;
+    }).reverse();
   }
-  sortByRadom(){
+  sortByRadom() {
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
     function shuffle(a) {
       for (let i = a.length; i; i--) {
-        let j = Math.floor(Math.random() * i);
+        const j = Math.floor(Math.random() * i);
         [a[i - 1], a[j]] = [a[j], a[i - 1]];
-        console.log("i是"+i);
-        console.log("j是"+j);
-        console.log(a[i-1],a[j]);
-        console.log([a[j], a[i - 1]]);
       }
     }
-    shuffle(this.users);
+    shuffle(this.programs);
   }
-  constructor(meta: Meta, title: Title) {
+  constructor(meta: Meta, title: Title, private programsList: ProgramsService) {
     title.setTitle('My Home Page');
 
-    meta.addTags([ 
+    meta.addTags([
       {
         name: 'author', content: 'eddic'
       },
@@ -103,9 +60,9 @@ saveNewUser(){
       {
         name: 'description', content: 'This is my great description.'
       },
-    ])
+    ]);
+    this.programs = this.programsList.getPrograms();
   }
-
   ngOnInit() {
   }
 
--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ProgramsService } from './programs.service';
+
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProgramsComponent } from './programs-list/programs.component';
+import { ProgramsDetailComponent } from './programs-detail/programs-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([
-      { path: '', component: HomePageComponent, pathMatch: 'full' }
+      { path: '', component: HomePageComponent, pathMatch: 'full' },
+      { path: 'programs-detail/:sid', component: ProgramsDetailComponent, pathMatch: 'full' }
     ])
   ],
-  declarations: [HomePageComponent]
+  declarations: [HomePageComponent, ProgramsComponent, ProgramsDetailComponent],
+  providers: [ProgramsService]
 })
 export class HomeModule { }
